e17a843f8915743e408b234406c1190e99fb99aa
Zeeshan Saleem <muhammadzeeshan00q@gmail.com>
2025-08-06 01:46:20 +0500

    Mega Blog Post Env Variables done

---
+++ MegaBlogProject/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">This is the Mega Project</h1>
+    </>
+  );
+}
+
+export default App;
