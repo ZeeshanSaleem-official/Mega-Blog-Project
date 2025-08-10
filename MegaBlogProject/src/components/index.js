fd08893714623583c5b77b9660baab4a9859cb4b
Zeeshan Saleem <muhammadzeeshan00q@gmail.com>
2025-08-10 16:12:02 +0500

    Components done

--- MegaBlogProject/src/components/index.js
+++ MegaBlogProject/src/components/index.js
@@ -1,7 +1,12 @@
 import Footer from "./Footer/Footer"
 import Header from "./Header/Header"
-
+import logo from "./logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import container from './container/container'
 export {
     Footer,
-    Header
+    Header,
+    logo,
+    LogoutBtn,
+    container
 }
---
+++ MegaBlogProject/src/components/input.jsx
@@ -0,0 +1,26 @@
+import React, { use, useId } from "react";
+
+const Input = React.forwardRef(function input(
+  { label, type = "text", classname = "", ...props },
+  ref
+) {
+  const id = useId;
+  return (
+    <div className=" w-full">
+      {label && (
+        <label className=" inline-block mb-1 pl-1" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <Input
+        type={type}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        {...props}
+        ref={ref}
+        id={id}
+      />
+    </div>
+  );
+});
+
+export default input;
